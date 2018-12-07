13a84dd6c31fd6ff9a0076da11d5e65c3f2f1642
Razi Rais <rbinrais@microsoft.com>
2018-12-07 13:33:33 -0500

    Minnor bug fixes.

--- reddit_news_scrap.py
+++ reddit_news_scrap.py
@@ -34,7 +34,7 @@ def contains_noise_words(comment):
 def is_200(response):
     return response.status_code == 200
 
-def exit_needed(response_recieved):
+def exit_needed(response_received):
     if not response_received:
         print("returned status_code", response.status_code, "200 was expected")
         sys.exit(0)
@@ -48,7 +48,6 @@ def articles_is_empty(articles):
 def get_articles():
     session = HTMLSession()
     url_reddit_news = "https://www.reddit.com/r/news/"
-    url_reddit_user_profile = "https://www.reddit.com/user"
     response = session.get(url_reddit_news)
     status = is_200(response)
     exit_needed(status)
@@ -61,11 +60,14 @@ def get_article_attributes(articles):
     Get the external,internal and user profile links 
     for each article along with its title.
     """
+    url_reddit_news_comments = "https://www.reddit.com/r/news/comments"
+    url_reddit_user_profile = "https://www.reddit.com/user"
+    article_list = []
     for article in articles:
         article_dict = {}
         article_dict["title"] = article.text.splitlines()[-2]
         for link in article.absolute_links:
-            if link.find(url_reddit_news) != -1:
+            if link.find(url_reddit_news_comments) != -1:
                 article_dict["url_reddit_internal"] = link
             elif link.find(url_reddit_user_profile) != -1:
                 article_dict["url_reddit_user_profile"] = link
@@ -74,26 +76,36 @@ def get_article_attributes(articles):
         article_list.append(article_dict)
     return article_list
 
-def get_comments(article_list):
+def save_comments_to_csv(article_list,directory):
     """
     Iterate over articles and get all the comments (till date)
     Each article corresponds to a separate csv file that stores all the comments posted on that article.
     Each comment is seperated by newline '/n' from other comments.
     CSV file name is based on the full url to the reddit article that is base64 encoded.
     """
+    session = HTMLSession()
     for article in article_list:
         print ("Fetching article --> " + article["url_reddit_internal"])
-        response = session.get(article["url_reddit_internal"])
-        status = is_200(response)
-        exit_needed(status)
-        r = response.html.find("p")
-        if r is not None:
-            # base64 encode the article internal url and use it as a filename. 
-            filename = '%s.csv' % base64.b64encode(article.url_reddit_internal.encode())
-            with open( directory+"/"+filename, 'w') as myfile:
-                for comments in r:
-                    if comments.full_text is not None and contains_noise_words(comments.full_text) == False:
-                        myfile.write(comments.full_text+ "\n")
+        try:
+            response = session.get(article["url_reddit_internal"])
+            status = is_200(response)
+            exit_needed(status)
+            r = response.html.find("p")pyth
+            if r is not None:
+                # base64 encode the article internal url and use it as a filename. 
+                filename = '%s.csv' % base64.b64encode(article["url_reddit_internal"].encode())
+                try:
+                    with open( directory+"/"+filename, 'w') as myfile:
+                        for comments in r:
+                            if comments.full_text is not None and contains_noise_words(comments.full_text) == False:
+                                myfile.write(comments.full_text+ "\n")
+                except Exception as e:
+                        print ("Error occurred while saving comments for the article --> ",article["url_reddit_internal"])
+                        print(e)
+
+        except Exception as e:
+                print ("Error occurred while fetching --> ",article["url_reddit_internal"])
+                print(e)
 
 def run_scraper(show_runtime=False):
     # Keep time to calculate how long it takes to complete this program.
@@ -111,7 +123,7 @@ def run_scraper(show_runtime=False):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    get_comments(article_list)
+    save_comments_to_csv(article_list,directory)
 
     if show_runtime == True:
         print('Total running time --> ' + str (datetime.now() - startTime))
