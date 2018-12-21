3ca94d6e36f23c3ae63dd4f98b565e060786e573
Eric Schles <eric.schles@microsoft.com>
2018-12-21 12:35:39 -0500

    adding auto labeling, ammended for change to wikipedia data source

--- automated_label_prop.py
+++ automated_label_prop.py
@@ -7,38 +7,30 @@ import pandas as pd
 import code
 from glob import glob
 
-def _combine_data(dfs):
-    df = pd.DataFrame()
-    for tmp in dfs:
-        df = df.append(tmp)
-    df.index = list(range(len(df)))
-    return df
-
 def _get_data():
-    csvs = glob("comments/*.csv")
-    dfs = [pd.read_csv(csv, index_col=False) for csv in csvs]
-    return _combine_data(dfs)
+    csv = "wikipedia/comments.csv"
+    return pd.read_csv(csv)
 
 def _clean_data(df):
-    return df[pd.notnull(df["comment"])]
+    return df[pd.notnull(df["comments"])]
 
 def _generate_features(df):
     vectorizer = TfidfVectorizer()
-    return vectorizer.fit_transform(df["comment"])
+    return vectorizer.fit_transform(df["comments"])
 
 def _encode_labels(x):
-    if x["labels"] == "L":
+    if x["labels"] == "syntax":
         return 0
-    if x["labels"] == "C":
+    if x["labels"] == "semantics":
         return 1
     if x["labels"] == -1:
         return -1
 
 def _decode_labels(x):
     if x == 0:
-        return "L"
+        return "syntax"
     if x == 1:
-        return "C"
+        return "semantics"
 
 def _generate_labels(df):    
     return df.apply(_encode_labels, axis=1)
@@ -46,6 +38,8 @@ def _generate_labels(df):
 def _label_propagation(df):
     X = _generate_features(df)
     labels = _generate_labels(df)
+    # for some reason pandas returns NaN for -1 values
+    labels = labels.fillna(-1)
     label_prop_model = LabelPropagation()
     label_prop_model.fit(X.toarray(), labels)
     return label_prop_model.predict(X.toarray())
